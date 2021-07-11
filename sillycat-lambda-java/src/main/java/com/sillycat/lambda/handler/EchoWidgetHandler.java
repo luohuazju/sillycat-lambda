fc42b67c122e426e396059878430c47921275740
Hua Luo <luohuazju@gmail.com>
2021-07-11 11:18:33 -0500

    java example

--- sillycat-lambda-java/src/main/java/com/sillycat/lambda/handler/EchoWidgetHandler.java
+++ sillycat-lambda-java/src/main/java/com/sillycat/lambda/handler/EchoWidgetHandler.java
@@ -1,17 +1,20 @@
 package com.sillycat.lambda.handler;
 
+import org.apache.log4j.Logger;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.sillycat.lambda.model.Widget;
 import com.sillycat.lambda.model.WidgetRequest;
 
-public class EchoWidgetHandler implements RequestHandler<WidgetRequest, Widget>
-{
+public class EchoWidgetHandler implements RequestHandler<WidgetRequest, Widget> {
+	
+	private static final Logger LOG = Logger.getLogger(EchoWidgetHandler.class);
 
-  @Override
-  public Widget handleRequest( WidgetRequest widgetRequest, Context context )
-  {
-    return new Widget( widgetRequest.getId(), "My Widget 3 " + widgetRequest.getId() );
-  }
+	public Widget handleRequest(WidgetRequest request, Context context) {
+		LOG.info("received: " + request);
+		Widget response = new Widget(request.getId(), "My Widget 3 " + request.getId());
+		return response;
+	}
 
 }
