cf215dcaeef234188d76fd2e79c06816019ae40b
Carl <luohuazju@gmail.com>
2023-01-03 20:37:15 -0600

    try to make 2023 version work

--- sillycat-lambda-java/src/main/java/com/sillycat/lambda/handler/ArticleGetHandler.java
+++ sillycat-lambda-java/src/main/java/com/sillycat/lambda/handler/ArticleGetHandler.java
@@ -1,33 +1,35 @@
 package com.sillycat.lambda.handler;
 
 import java.util.HashMap;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.sillycat.lambda.model.Article;
 import com.sillycat.lambda.service.ArticleService;
 
-public class ArticleGetHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+public class ArticleGetHandler implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
 
 	private ArticleService articleService = new ArticleService();
 	private Gson gson = new GsonBuilder().setPrettyPrinting().create();
+	private static final Logger logger = LogManager.getLogger(ArticleGetHandler.class);
 
 	@Override
-	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent event, Context context) {
-		LambdaLogger logger = context.getLogger();
-		logger.log("event: " + gson.toJson(event));
-		logger.log("path: " + event.getPath());
-		logger.log("pathParameters: " + event.getPathParameters());
+	public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent event, Context context) {
+		
+		logger.info("event: " + gson.toJson(event));
+		logger.info("path: " + event.getRawPath());
+		logger.info("pathParameters: " + event.getPathParameters());
 		String id = event.getPathParameters().get("id");
-		logger.log("id: " + id);
+		logger.info("id: " + id);
 		Article item = articleService.get(id);
 
-		APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+		APIGatewayV2HTTPResponse response = new APIGatewayV2HTTPResponse();
 		response.setIsBase64Encoded(false);
 		response.setStatusCode(200);
 		HashMap<String, String> headers = new HashMap<String, String>();
--- sillycat-lambda-java/src/main/java/com/sillycat/lambda/handler/ArticlePostHandler.java
+++ sillycat-lambda-java/src/main/java/com/sillycat/lambda/handler/ArticlePostHandler.java
@@ -2,28 +2,31 @@ package com.sillycat.lambda.handler;
 
 import java.util.HashMap;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.sillycat.lambda.model.Article;
 import com.sillycat.lambda.model.ArticleRequest;
 import com.sillycat.lambda.service.ArticleService;
 
-public class ArticlePostHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+public class ArticlePostHandler implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
 
 	private ArticleService articleService = new ArticleService();
 	private Gson gson = new GsonBuilder().setPrettyPrinting().create();
+	
+	private static final Logger logger = LogManager.getLogger(ArticlePostHandler.class);
 
 	@Override
-	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent event, Context context) {
-		LambdaLogger logger = context.getLogger();
-		logger.log("event: " + gson.toJson(event));
+	public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent event, Context context) {
+		logger.info("event: " + gson.toJson(event));
 		ArticleRequest request = gson.fromJson(event.getBody(), ArticleRequest.class);
-		logger.log("request: " + request);
+		logger.info("request: " + request);
 
 		Article articleRequest = new Article();
 		articleRequest.setAuthor(request.getAuthor());
@@ -31,7 +34,7 @@ public class ArticlePostHandler implements RequestHandler<APIGatewayProxyRequest
 		articleRequest.setTitle(request.getTitle());
 		Article articleResponse = articleService.create(articleRequest);
 
-		APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+		APIGatewayV2HTTPResponse response = new APIGatewayV2HTTPResponse();
 		response.setIsBase64Encoded(false);
 		response.setStatusCode(200);
 		HashMap<String, String> headers = new HashMap<String, String>();
